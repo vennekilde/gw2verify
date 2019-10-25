81f21cf0560accaa28598f41e0b60e38b4c09dc0
Jeppe Vennekilde <vennekilde@outlook.com>
2019-10-25 20:13:59 +0200

    Changed link to underworld

--- pkg/verify/status.go
+++ pkg/verify/status.go
@@ -85,7 +85,7 @@ type Configuration struct {
 }
 
 var Config = Configuration{
-	LinkedWorlds: []int{2105},
+	LinkedWorlds: []int{2006},
 }
 
 func Status(serviceID int, serviceUserID string) (status VerificationStatusExt, link ServiceLink) {
