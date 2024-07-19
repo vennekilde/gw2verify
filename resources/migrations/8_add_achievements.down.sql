29f245f04b1164f8ad2931abf50bc0c5d7243b44
Vennekilde <vennekilde@outlook.com>
2024-07-20 01:35:21 +0200

    Fixed migration scripts

--- resources/migrations/8_add_achievements.down.sql
+++ resources/migrations/8_add_achievements.down.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "activities" (
     "id" serial NOT NULL,
-    "account_id" uuid NOT NULL,
+    "account_id" character varying(64) NOT NULL,
     "timestamp" timestamptz DEFAULT CURRENT_TIMESTAMP NOT NULL,
     "rank" smallint NOT NULL,
     "kills" int NOT NULL,
--- resources/migrations/8_add_achievements.up.sql
+++ resources/migrations/8_add_achievements.up.sql
@@ -1,3 +1,15 @@
+ALTER TABLE "histories" DROP CONSTRAINT "histories_account_id_fkey";
+ALTER TABLE "token_infos" DROP CONSTRAINT "token_infos_account_id_fkey";
+
+ALTER TABLE "accounts" ALTER "id" TYPE uuid USING id::uuid;
+ALTER TABLE "bans" ALTER "account_id" TYPE uuid USING account_id::uuid;
+ALTER TABLE "histories" ALTER "account_id" TYPE uuid USING account_id::uuid;
+ALTER TABLE "token_infos" ALTER "account_id" TYPE uuid USING account_id::uuid;
+ALTER TABLE "activities" ALTER "account_id" TYPE uuid USING account_id::uuid;
+
+ALTER TABLE "token_infos" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "histories" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
 CREATE TABLE "achievements" (
     "id" serial NOT NULL,
     "account_id" uuid NOT NULL,
@@ -6,6 +18,7 @@ CREATE TABLE "achievements" (
     "value" int NOT NULL,
     PRIMARY KEY ("id")
 );
+ALTER TABLE "achievements" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
 CREATE INDEX "achievements_achievement" ON "achievements" ("achievement");
 CREATE INDEX "achievements_account_id_achievement" ON "achievements" ("account_id", "achievement");
 
@@ -15,14 +28,3 @@ INSERT INTO "achievements" ("account_id", "timestamp", "achievement", "value")
     SELECT "account_id", "timestamp", -1, "rank" FROM "activities";
 
 DROP TABLE activities;
-
-ALTER TABLE "histories" DROP CONSTRAINT "histories_account_id_fkey";
-ALTER TABLE "token_infos" DROP CONSTRAINT "token_infos_account_id_fkey";
-
-ALTER TABLE "accounts" ALTER "id" TYPE uuid USING id::uuid;
-ALTER TABLE "bans" ALTER "account_id" TYPE uuid USING account_id::uuid;
-ALTER TABLE "histories" ALTER "account_id" TYPE uuid USING account_id::uuid;
-ALTER TABLE "token_infos" ALTER "account_id" TYPE uuid USING account_id::uuid;
-
-ALTER TABLE "token_infos" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id") ON DELETE CASCADE ON UPDATE NO ACTION;
-ALTER TABLE "histories" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
